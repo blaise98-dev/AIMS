be656e055823f20d8ebcf9c9f5db620d117acc6e
Blaise <bnindenkimana2@gmail.com>
2024-11-18 01:19:56 +0000

    User Interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,15 +39,16 @@ label_encoder = LabelEncoder()
 label_encoder.fit(labels)
 
 # Function to predict disease
-def predict_disease(symptoms_list, model, top_k=5):
+def predict_disease(symptoms_list, model):
     encoded_symptoms = [severity_map.get(symptom, 0) for symptom in symptoms_list]
     for i in range(len(encoded_symptoms), 17):
         encoded_symptoms.append(0)
     symptoms_array = np.array(encoded_symptoms).reshape(1, -1)
     disease_probs = model.predict_proba(symptoms_array)[0]
-    sorted_probs_idx = np.argsort(disease_probs)[::-1]
-    top_k_diseases = [(label_encoder.inverse_transform([idx])[0], round(disease_probs[idx], 2)) for idx in sorted_probs_idx[:top_k]]
-    return top_k_diseases
+    top_disease_idx = np.argmax(disease_probs)
+    top_disease = label_encoder.inverse_transform([top_disease_idx])[0]
+    top_probability = round(disease_probs[top_disease_idx], 2)
+    return top_disease, top_probability
 
 # Streamlit app
 st.title('Disease Prediction System')
@@ -58,25 +59,15 @@ selected_symptoms = st.multiselect('Select your symptoms', symptoms)
 # Button to predict disease
 if st.button('Predict Disease'):
     if selected_symptoms:
-        top_k_diseases = predict_disease(selected_symptoms, rf, top_k=5)
-        st.write("Predicted diseases with probabilities:", top_k_diseases)
-        predict_diseases = [disease[0] for disease in top_k_diseases]
-        predict_probs = [disease[1] for disease in top_k_diseases]
-        sns.set_style("whitegrid")
-        sns.set_palette("Set1")
-        sns.barplot(x=predict_diseases, y=predict_probs)
-        plt.xlabel("Different Diseases")
-        plt.ylabel("Probability")
-        plt.title("Predicted Diseases and their Probabilities")
-        plt.tight_layout()
-        st.pyplot(plt)
+        top_disease, top_probability = predict_disease(selected_symptoms, rf)
+        st.write(f"**Predicted Disease:** {top_disease}")
+        st.write(f"**Probability:** {top_probability}")
         
         # Display the precautionary measures
         st.write("### Precautionary Measures:")
-        for disease, _ in top_k_diseases:
-            if disease in precaution_dict:
-                st.write(f"**{disease}:**")
-                for precaution in precaution_dict[disease]:
-                    st.write(f"- {precaution}")
+        if top_disease in precaution_dict:
+            st.write(f"**{top_disease}:**")
+            for precaution in precaution_dict[top_disease]:
+                st.write(f"- {precaution}")
     else:
         st.write("Please select at least one symptom.")
